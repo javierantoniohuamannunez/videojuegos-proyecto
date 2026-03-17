04766d99cb33c28dde3efa240b20b54b31877871
javier huaman <javierantonio_huamannunez@iescarlesvallbona.cat>
2026-03-17 19:05:19 +0100

    se corregio los modelos de dungeos y de bosses

--- backend/models/Dungeon.js
+++ backend/models/Dungeon.js
@@ -1,17 +1,56 @@
 const mongoose = require("mongoose")
 
 const dungeonSchema = new mongoose.Schema({
+
+  // Nombre de la dungeon 
   nombre: {
     type: String,
     required: true
   },
-  ubicacion: String,
+
+  // lugar del en el mapa
+  ubicacion: {
+    type: String,
+    required: true
+  },
+
   dificultad: {
     type: String,
-    enum: ["facil", "normal", "dificil"]
+    enum: ["facil", "normal", "dificil"], 
+    required: true
   },
-  bossFinal: String,
-  nivelRecomendado: Number,
+
+  // Boss final de la dungeon
+  bossFinal: {
+    type: String,
+    required: true
+  },
+
+  // Nivel recomendado para entrar
+  nivelRecomendado: {
+    type: Number,
+    required: true,
+    min: 20
+  },
+
+  // Indica si la dungeon está activa
+  activo: {
+    type: Boolean,
+    default: false
+  },
+
+  // Fecha de creacion
+  fechaCreacion: {
+    type: Date,
+    default: Date.now
+  },
+
+  // Recompensas del dungeon
+  recompensas: {
+    type: [String],
+    default: []
+  },
+
   imagen: String
 
 })
