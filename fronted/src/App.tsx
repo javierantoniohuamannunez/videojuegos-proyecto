bdd9a781af84d2a501a348ecabebcc2fd453e713
javier huaman <javierantonio_huamannunez@iescarlesvallbona.cat>
2026-03-22 22:41:50 +0100

    se agrego un navbar para bosses y dungeos

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,19 +1,17 @@
 import BossesPage from "./pages/BossesPage";
 import DungeonsPage from "./pages/DungeonsPage";
-import { useState } from "react"
-
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 export default function App() {
-  const [vista, setVista] = useState<"bosses" | "dungeons">("bosses")
+  const [vista, setVista] = useState<"bosses" | "dungeons">("bosses");
 
   return (
     <div>
       <h1>Throne & Liberty</h1>
 
-      <button onClick={() => setVista("bosses")}>Bosses</button>
-      <button onClick={() => setVista("dungeons")}>Dungeons</button>
-
+      <Navbar vista={vista} setVista={setVista} />
       {vista === "bosses" && <BossesPage />}
       {vista === "dungeons" && <DungeonsPage />}
     </div>
-  )
+  );
 }
